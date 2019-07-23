781d0f85ffbbda785da5420775f73253d65a6bb7
nijikokun <nijikokun@gmail.com>
2019-07-22 20:14:11 -0700

    feat(*) add phase targeting for functions

--- kong/plugins/pre-function/_handler.lua
+++ kong/plugins/pre-function/_handler.lua
@@ -1,20 +1,22 @@
 -- handler file for both the pre-function and post-function plugin
 return function(plugin_name, priority)
-  local loadstring = loadstring
   local insert = table.insert
-  local ipairs = ipairs
+  
 
   local config_cache = setmetatable({}, { __mode = "k" })
+  local ServerlessFunction = {}
 
   local ServerlessFunction = {
     PRIORITY = priority,
     VERSION = "0.3.1",
   }
 
-  function ServerlessFunction:access(config)
+  function invoke(phase, config)
+    if (config.phase ~= phase) then
+      return
+    end
 
     local functions = config_cache[config]
-
     if not functions then
       -- first call, go compile the functions
       functions = {}
@@ -40,6 +42,38 @@ return function(plugin_name, priority)
     for _, fn in ipairs(functions) do
       fn()
     end
+  end
+
+  function ServerlessFunction:new()
+    ServerlessFunction.super.new(self, "ServerlessFunction:" .. plugin_name)
+  end
+
+  function ServerlessFunction:init_worker(config)
+    invoke("init_worker", config)
+  end
+  
+  function ServerlessFunction:certificate(config)
+    invoke("certificate", config)
+  end
+  
+  function ServerlessFunction:rewrite(config)
+    invoke("rewrite", config)
+  end
+
+  function ServerlessFunction:access(config)
+    invoke("access", config)
+  end
+
+  function ServerlessFunction:header_filter(config)
+    invoke("header_filter", config)
+  end
+
+  function ServerlessFunction:body_filter(config)
+    invoke("body_filter", config)
+  end
+
+  function ServerlessFunction:log(config)
+    invoke("log", config)
   end
 
 
