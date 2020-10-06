5682c9da46edda293078b9552200dcbb0b062c1e
Javier Guerra <javier.guerra@konghq.com>
2020-10-05 22:37:29 -0500

    tests - add tests for buffered request and/or response

--- spec/04-phases_spec.lua
+++ spec/04-phases_spec.lua
@@ -16,71 +16,73 @@ local mock_one_fn = [[
 
 
 for _, plugin_name in ipairs({ "pre-function", "post-function" }) do
+  for buffering_request, buffering_response, buffering_label in helpers.each_buffering() do
+    describe("Plugin: " .. plugin_name .."(".. buffering_label ..")", function()
 
-  describe("Plugin: " .. plugin_name, function()
+      setup(function()
+        local bp, db = helpers.get_db_utils()
 
-    setup(function()
-      local bp, db = helpers.get_db_utils()
+        assert(db:truncate())
 
-      assert(db:truncate())
+        local service = bp.services:insert {
+          name     = "service-1",
+          host     = helpers.mock_upstream_host,
+          port     = helpers.mock_upstream_port,
+        }
 
-      local service = bp.services:insert {
-        name     = "service-1",
-        host     = helpers.mock_upstream_host,
-        port     = helpers.mock_upstream_port,
-      }
+        bp.routes:insert {
+          service = { id = service.id },
+          hosts   = { "one." .. plugin_name .. ".com" },
+          request_buffering = buffering_request,
+          response_buffering = buffering_response,
+        }
 
-      bp.routes:insert {
-        service = { id = service.id },
-        hosts   = { "one." .. plugin_name .. ".com" },
-      }
-
-      local config = {}
-      for _, phase in ipairs({ "certificate", "rewrite", "access",
-                               "header_filter", "body_filter", "log"}) do
-        config[phase] = {}
-        for i, index in ipairs({"first", "second", "third"}) do
-          config[phase][i] = mock_one_fn:format(plugin_name, phase, index)
+        local config = {}
+        for _, phase in ipairs({ "certificate", "rewrite", "access",
+                                 "header_filter", "body_filter", "log"}) do
+          config[phase] = {}
+          for i, index in ipairs({"first", "second", "third"}) do
+            config[phase][i] = mock_one_fn:format(plugin_name, phase, index)
+          end
         end
-      end
 
-      bp.plugins:insert {
-        name    = plugin_name,
-        config  = config,
-      }
+        bp.plugins:insert {
+          name    = plugin_name,
+          config  = config,
+        }
 
-      assert(helpers.start_kong({
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-      }))
-    end)
+        assert(helpers.start_kong({
+          nginx_conf = "spec/fixtures/custom_nginx.template",
+        }))
+      end)
 
-    teardown(function()
-      helpers.stop_kong()
-    end)
+      teardown(function()
+        helpers.stop_kong()
+      end)
 
 
-    it("hits all phases, with 3 functions, on 3 requests", function()
-      local filename = "/tmp/" .. plugin_name .. "_output"
-      os.remove(filename)
+      it("hits all phases, with 3 functions, on 3 requests", function()
+        local filename = "/tmp/" .. plugin_name .. "_output"
+        os.remove(filename)
 
-      for i = 1,3 do
-        local client = helpers.proxy_ssl_client()
+        for i = 1,3 do
+          local client = helpers.proxy_ssl_client()
 
-        local res = assert(client:send {
-          method = "GET",
-          path = "/status/200",
-          headers = {
-            ["Host"] = "one." .. plugin_name .. ".com"
-          }
-        })
-        assert.response(res).has.status(200)
+          local res = assert(client:send {
+            method = "GET",
+            path = "/status/200",
+            headers = {
+              ["Host"] = "one." .. plugin_name .. ".com"
+            }
+          })
+          assert.response(res).has.status(200)
 
-        client:close()
-        ngx.sleep(0.1) -- wait for log-phase handler to execute
-      end
+          client:close()
+          ngx.sleep(0.1) -- wait for log-phase handler to execute
+        end
 
-      local content = require("pl.utils").readfile(filename)
-      assert.equal(([[
+        local content = require("pl.utils").readfile(filename)
+        assert.equal(([[
 phase: 'certificate', index: 'first', plugin: 'pre-function'
 phase: 'certificate', index: 'second', plugin: 'pre-function'
 phase: 'certificate', index: 'third', plugin: 'pre-function'
@@ -145,6 +147,7 @@ phase: 'log', index: 'first', plugin: 'pre-function'
 phase: 'log', index: 'second', plugin: 'pre-function'
 phase: 'log', index: 'third', plugin: 'pre-function'
 ]]):gsub("pre%-function", plugin_name),content)
+      end)
     end)
-  end)
+  end
 end
